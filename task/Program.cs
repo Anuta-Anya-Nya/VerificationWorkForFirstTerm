19d86284a039ab46caa9a76f556a24df62d8b350
Aeshe <syslichek@mail.ru>
2022-09-17 00:07:22 +0300

    add a solutionof the task

--- task/Program.cs
+++ task/Program.cs
@@ -1,25 +1,43 @@
-﻿string[] array = {"mama", "papa", "ann", "^-)"};
-int count=0;
-for (int i = 0; i < array.Length; i++)
+﻿string[] FillArray(int size)
 {
-    Console.WriteLine(array[i]);
-    if(array[i].Length<=3)
+    string[] anyArray = new string[size];
+    for (int i = 0; i < size; i++)
     {
-        count++;
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        anyArray[i] = Console.ReadLine()!;
     }
+    return anyArray;
 }
-string[] newArray = new string[count];
-count=0;
-for (int i = 0; i < array.Length; i++)
-{    
-    if(array[i].Length<=3)
-    {
-        newArray[count]=array[i];
-        count++;        
-    }    
-}
-Console.WriteLine();
-for (int i = 0; i < newArray.Length; i++)
+
+Console.Write("Введите длину массива: ");
+if (!int.TryParse(Console.ReadLine(), out int sizeArray))
 {
-    Console.WriteLine(newArray[i]);
+    Console.WriteLine("Введено не число!");
+    return;
 }
+string[] array = FillArray(sizeArray);
+string[] FilterArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] newArr = new string[count];
+    count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[count] = arr[i];
+            count++;
+        }
+    }
+    return newArr;
+}
+string[] newArray = FilterArray(array);
+Console.WriteLine($"Введенный массив: [{String.Join(", ", array)}]");
+Console.WriteLine($"Полученный массив: [{String.Join(", ", newArray)}]");
