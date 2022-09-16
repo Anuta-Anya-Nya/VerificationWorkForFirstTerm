62fa95152b3977916c9e71acac9a2a169ee73130
Aeshe <syslichek@mail.ru>
2022-09-16 15:01:11 +0300

    Добавила неоптимальное решение, которое первое пришло в голову

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"mama", "papa", "ann", "^-)"};
+int count=0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+    if(array[i].Length<=3)
+    {
+        count++;
+    }
+}
+string[] newArray = new string[count];
+count=0;
+for (int i = 0; i < array.Length; i++)
+{    
+    if(array[i].Length<=3)
+    {
+        newArray[count]=array[i];
+        count++;        
+    }    
+}
+Console.WriteLine();
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.WriteLine(newArray[i]);
+}
